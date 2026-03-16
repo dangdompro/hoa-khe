3d8331d9cdc7f0e25371103e981769732203bb7e
Dang Dom <dangdompro@gmail.com>
2026-03-16 15:30:51 +0700

    Finish homepage layout and assets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,72 @@
 console.log("JS connected");
 
-const slides = document.querySelectorAll(".slide");
+// =========================
+// HOME ABOUT SLIDER
+// =========================
+const aboutSlider = document.querySelector(".home-about-slider");
 
-let index = 0;
+if (aboutSlider) {
+  const slides = aboutSlider.querySelectorAll(".home-about-slide");
 
-setInterval(() => {
-  slides[index].classList.remove("active");
+  let current = 0;
 
-  index = (index + 1) % slides.length;
+  function showSlide(index) {
+    slides.forEach((slide) => slide.classList.remove("active"));
 
-  slides[index].classList.add("active");
-}, 4000);
+    slides[index].classList.add("active");
+
+    current = index;
+  }
+
+  function nextSlide() {
+    let next = current + 1;
+
+    if (next >= slides.length) next = 0;
+
+    showSlide(next);
+  }
+
+  setInterval(nextSlide, 5000);
+  showSlide(0);
+}
+
+// ============================
+// HOME PROJECT SLIDER
+// ============================
+
+const projectSlider = document.querySelector(".home-project-slider");
+
+if (projectSlider) {
+  const slides = projectSlider.querySelectorAll(".home-project-slide");
+  const dots = document.querySelectorAll(".project-dot");
+
+  let current = 0;
+
+  function showProjectSlide(index) {
+    slides.forEach((slide) => slide.classList.remove("active"));
+    dots.forEach((dot) => dot.classList.remove("active"));
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+
+    current = index;
+  }
+
+  function nextProjectSlide() {
+    let next = current + 1;
+
+    if (next >= slides.length) next = 0;
+
+    showProjectSlide(next);
+  }
+
+  setInterval(nextProjectSlide, 5000);
+
+  dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      showProjectSlide(index);
+    });
+  });
+
+  showProjectSlide(0);
+}
